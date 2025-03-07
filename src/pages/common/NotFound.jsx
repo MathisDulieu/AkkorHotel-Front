77164681b48ffb51318b432ea7f2e3479c2f891c
Mathis <106063734+MathisDulieu@users.noreply.github.com>
2025-03-07 15:19:46 +0100

    Adding home page with register and login page

--- src/pages/common/NotFound.jsx
+++ src/pages/common/NotFound.jsx
@@ -1,9 +1,12 @@
-const NotFound = () => {
-    return (
-        <div>
+import React, { useEffect } from 'react';
+import { Navigate } from 'react-router-dom';
 
-        </div>
-    );
+const NotFound = () => {
+    useEffect(() => {
+        console.log("Page introuvable");
+    }, []);
+
+    return <Navigate to="/" replace />;
 };
 
 export default NotFound;
